8c23a8b14db418c3cd7f2e7abe0cb46e8cff7b95
max-dnz <maxime.donzeau@gmail.com>
2022-07-21 14:08:29 -0700

    Revert "pb solving" 21th July - Version with EFIELD This reverts commit 0bd48280490c3fb9fc2cbe18f4738acd39bbb259.

--- Exec/Efield/FlameSheetIons/pelelm_prob.cpp
+++ Exec/Efield/FlameSheetIons/pelelm_prob.cpp
@@ -10,8 +10,15 @@ void PeleLM::readProbParm()
    pp.query("standoff", PeleLM::prob_parm->standoff);
    pp.query("pertmag",  PeleLM::prob_parm->pertmag);
 
+<<<<<<< HEAD
    pp.query("PhiV_y_hi", PeleLM::prob_parm->phiV_hiy);
    pp.query("PhiV_y_lo", PeleLM::prob_parm->phiV_loy);
+=======
+#ifdef PELE_USE_EFIELD
+   pp.query("PhiV_y_hi", PeleLM::prob_parm->phiV_hiy);
+   pp.query("PhiV_y_lo", PeleLM::prob_parm->phiV_loy);
+#endif
+>>>>>>> parent of 0bd4828... pb solving
 
    PeleLM::pmf_data.initialize();
 }
--- Exec/Efield/IonizedAirWave/pelelm_prob.cpp
+++ Exec/Efield/IonizedAirWave/pelelm_prob.cpp
@@ -1,6 +1,9 @@
 #include <PeleLM.H>
 #include <AMReX_ParmParse.H>
+<<<<<<< HEAD
 #include <pmf.H>
+=======
+>>>>>>> parent of 0bd4828... pb solving
 
 void PeleLM::readProbParm()
 {
@@ -14,8 +17,12 @@ void PeleLM::readProbParm()
    pp.query("PhiV_y_hi", PeleLM::prob_parm->phiV_hiy);
    pp.query("PhiV_y_lo", PeleLM::prob_parm->phiV_loy);
 
+<<<<<<< HEAD
    std::string pmf_datafile;
    pp.query("pmf_datafile", pmf_datafile);
    int pmf_do_average = 1;
    PMF::read_pmf(pmf_datafile, pmf_do_average);
+=======
+   PeleLM::pmf_data.initialize();
+>>>>>>> parent of 0bd4828... pb solving
 }
