cb803174b914fe3c2aef7dc0a5d1086c38dd7e81
max-dnz <maxime.donzeau@gmail.com>
2022-07-29 13:05:40 -0700

    Set experimental setup + Tests

--- Exec/MyCases/E-FIELD/pelelm_prob.cpp
+++ Exec/MyCases/E-FIELD/pelelm_prob.cpp
@@ -6,8 +6,6 @@ void PeleLM::readProbParm()
    amrex::ParmParse pp("prob");
    
    std::string type;
-   pp.query("standoff", PeleLM::prob_parm->standoff);
-   pp.query("pertmag",  PeleLM::prob_parm->pertmag);
    
    //Definition of the Geometry
    pp.query("Xmax", prob_parm->Xmax);
@@ -44,6 +42,7 @@ void PeleLM::readProbParm()
 #ifdef PELE_USE_EFIELD
    pp.query("PhiV_y_hi", PeleLM::prob_parm->phiV_hiy);
    pp.query("PhiV_y_lo", PeleLM::prob_parm->phiV_loy);
+   
 #endif
 
 }
